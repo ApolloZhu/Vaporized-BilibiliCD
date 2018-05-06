8f906eacc2ae711c5047db3e81fcdfeb62c07098
Zhiyu Zhu/朱智语 <public-apollonian@outlook.com>
2018-05-06 15:11:57 -0400

    Vapor Web Template

---
+++ Sources/Run/main.swift
@@ -0,0 +1,34 @@
+import App
+
+/// We have isolated all of our App's logic into
+/// the App module because it makes our app
+/// more testable.
+///
+/// In general, the executable portion of our App
+/// shouldn't include much more code than is presented
+/// here.
+///
+/// We simply initialize our Droplet, optionally
+/// passing in values if necessary
+/// Then, we pass it to our App's setup function
+/// this should setup all the routes and special
+/// features of our app
+///
+/// .run() runs the Droplet's commands,
+/// if no command is given, it will default to "serve"
+
+print("""
+    Vaporized BilibiliCD.  Copyright (C) 2018  Zhiyu Zhu
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+    
+    """)
+
+let config = try Config()
+try config.setup()
+
+let drop = try Droplet(config)
+try drop.setup()
+
+try drop.run()
