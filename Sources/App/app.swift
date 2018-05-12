9141163c5c665ce9de38bfa43643d560b9047c6c
ApolloZhu <public-apollonian@outlook.com>
2018-05-11 22:36:08 -0400

    Register ErrorMiddleware

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -5,6 +5,7 @@ public func app(_ env: Environment) throws -> Application {
     var config = Config.default()
     var env = env
     var services = Services.default()
+    services.register(ErrorMiddleware.self)
     try configure(&config, &env, &services)
     let app = try Application(config: config, environment: env, services: services)
     try boot(app)
