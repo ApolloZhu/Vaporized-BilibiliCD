4dc1c0e5084c18dbbeda9e05fd0f09c84d618adb
ApolloZhu <public-apollonian@outlook.com>
2018-05-11 22:13:07 -0400

    Replace Error JSON Format

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,106 @@
 import Leaf
 import Vapor
 
+/// Captures all errors and transforms them into an internal server error HTTP response.
+public final class ErrorMiddleware: Middleware, ServiceType {
+    /// See `ServiceType`.
+    public static func makeService(for worker: Container) throws -> ErrorMiddleware {
+        return try .default(environment: worker.environment, log: worker.make())
+    }
+    
+    /// Create a default `ErrorMiddleware`. Logs errors to a `Logger` based on `Environment`
+    /// and converts `Error` to `Response` based on conformance to `AbortError` and `Debuggable`.
+    ///
+    /// - parameters:
+    ///     - environment: The environment to respect when presenting errors.
+    ///     - log: Log destination.
+    public static func `default`(environment: Environment, log: Logger) -> ErrorMiddleware {
+        /// Structure of `ErrorMiddleware` default response.
+        struct ErrorResponse: Encodable {
+            var error: UInt
+            
+            /// The reason for the error.
+            var reason: String
+        }
+        
+        return .init { req, error in
+            // log the error
+            log.report(error: error, verbose: !environment.isRelease)
+            
+            // variables to determine
+            let status: HTTPResponseStatus
+            let reason: String
+            let headers: HTTPHeaders
+            
+            // inspect the error type
+            switch error {
+            case let abort as AbortError:
+                // this is an abort error, we should use its status, reason, and headers
+                reason = abort.reason
+                status = abort.status
+                headers = abort.headers
+            case let validation as ValidationError:
+                // this is a validation error
+                reason = validation.reason
+                status = .badRequest
+                headers = [:]
+            case let debuggable as Debuggable where !environment.isRelease:
+                // if not release mode, and error is debuggable, provide debug
+                // info directly to the developer
+                reason = debuggable.reason
+                status = .internalServerError
+                headers = [:]
+            default:
+                // not an abort error, and not debuggable or in dev mode
+                // just deliver a generic 500 to avoid exposing any sensitive error info
+                reason = "Something went wrong."
+                status = .internalServerError
+                headers = [:]
+            }
+            
+            // create a Response with appropriate status
+            let res = req.makeResponse(http: .init(status: status, headers: headers))
+            
+            // attempt to serialize the error to json
+            do {
+                let errorResponse = ErrorResponse(error: status.code, reason: reason)
+                res.http.body = try HTTPBody(data: JSONEncoder().encode(errorResponse))
+                res.http.headers.replaceOrAdd(name: .contentType, value: "application/json; charset=utf-8")
+            } catch {
+                res.http.body = HTTPBody(string: "Oops: \(error)")
+                res.http.headers.replaceOrAdd(name: .contentType, value: "text/plain; charset=utf-8")
+            }
+            return res
+        }
+    }
+    
+    /// Error-handling closure.
+    private let closure: (Request, Error) -> (Response)
+    
+    /// Create a new `ErrorMiddleware`.
+    ///
+    /// - parameters:
+    ///     - closure: Error-handling closure. Converts `Error` to `Response`.
+    public init(_ closure: @escaping (Request, Error) -> (Response)) {
+        self.closure = closure
+    }
+    
+    /// See `Middleware`.
+    public func respond(to req: Request, chainingTo next: Responder) throws -> Future<Response> {
+        let response: Future<Response>
+        do {
+            response = try next.respond(to: req)
+        } catch {
+            response = req.eventLoop.newFailedFuture(error: error)
+        }
+        
+        return response.mapIfError { error in
+            return self.closure(req, error)
+        }
+    }
+}
+
+
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register routes to the router
@@ -10,7 +110,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
